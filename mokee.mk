6982ca00a9074a7ab29c2ce9d8bbc5f229d2dcf8
Ryuinferno <ryuinferno.xda@gmail.com>
2014-06-25 05:57:21 +0200

    p760: MoKee bringup

--- mokee.mk
+++ mokee.mk
@@ -1,13 +1,13 @@
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit device configuration for LG Optimus L9 P760
 $(call inherit-product, device/lge/p760/full_p760.mk)
 
 # This device has NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_p760
+PRODUCT_NAME := mk_p760
 PRODUCT_BRAND := lg
 PRODUCT_DEVICE := p760
 PRODUCT_MODEL := LG-P760
@@ -18,7 +18,7 @@ PRODUCT_SFX := intl
 # Release name and versioning
 PRODUCT_RELEASE_NAME := p760
 PRODUCT_VERSION_DEVICE_SPECIFIC := 
--include vendor/cm/config/common_versions.mk
+-include vendor/mk/config/common_versions.mk
 
 UTC_DATE := $(shell date +%s)
 
